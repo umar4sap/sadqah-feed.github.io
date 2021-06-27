bd07cb6bd79b092780321214cceef6aa31cfd6cd
umar2code <m.jumar@hotmail.com>
2021-06-27 22:48:44 +0400

    added

---
+++ precache-manifest.9cafaaccb3cea2a78608411c6a527311.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6abbcfbd7ed392e441a3b3a2e0c868ed",
+    "url": "/feedApp/index.html"
+  },
+  {
+    "revision": "05c30115f04664152811",
+    "url": "/feedApp/static/css/2.799c978e.chunk.css"
+  },
+  {
+    "revision": "8b98aeb581a76638cce3",
+    "url": "/feedApp/static/css/main.a180a5c9.chunk.css"
+  },
+  {
+    "revision": "05c30115f04664152811",
+    "url": "/feedApp/static/js/2.8638b4db.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/feedApp/static/js/2.8638b4db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655590fbbecc1ef080f5",
+    "url": "/feedApp/static/js/3.73726b27.chunk.js"
+  },
+  {
+    "revision": "8b98aeb581a76638cce3",
+    "url": "/feedApp/static/js/main.17a8ed82.chunk.js"
+  },
+  {
+    "revision": "bee9142b50dbba2c81a5",
+    "url": "/feedApp/static/js/runtime-main.94907a64.js"
+  },
+  {
+    "revision": "e475dea614a0994756e062cf890cad59",
+    "url": "/feedApp/static/media/avatar.e475dea6.png"
+  },
+  {
+    "revision": "272a483aa6a581b0fecbf1897acbab71",
+    "url": "/feedApp/static/media/event.272a483a.jpeg"
+  },
+  {
+    "revision": "bdb1afe6465bc1fc620e91db28bccfd2",
+    "url": "/feedApp/static/media/logo.bdb1afe6.png"
+  }
+]);
